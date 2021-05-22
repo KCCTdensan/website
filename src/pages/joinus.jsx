44d70f985d0ce65bae6a6bf014bb0e50bcfd0201
YamaD <me@dyama.net>
2021-05-22 14:10:32 +0900

    FAQのスタイルを弄った

--- src/pages/joinus.jsx
+++ src/pages/joinus.jsx
@@ -4,16 +4,51 @@ import styled from "styled-components"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const Faq = styled.details`
-`
-const Q = styled.summary`
-  ::before {
-    content: "Q. ";
+const FaqList = styled.div`
+  details {
+    margin: 16px 0;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-shadow: 0 0 8px 2px #f4f4f4;
+  }
+  details[open] {
+    margin: 18px 0;
+    box-shadow: 0 0 16px 4px #eee;
+  }
+  details[open] summary {
+    border-bottom: 1px solid #ccc;
   }
 `
-const A = styled.span`
+const Q = styled.summary`
+  padding: 12px;
+  user-select: none;
+  font-weight: bolder;
+
+  ::marker {
+    color: gray;
+    font-size: 1em;
+  }
   ::before {
-    content: "A. ";
+    content: "Q.";
+    width: 1.5em;
+    display: inline-block;
+    font-weight: bold;
+    font-size: 1.3em;
+    line-height: 1em;
+  }
+`
+const A = styled.div`
+  padding: 12px;
+  padding-left: 4em;
+
+  ::before {
+    content: "A.";
+    width: 1.5em;
+    margin-left: -1.5em;
+    display: inline-block;
+    font-weight: bold;
+    font-size: 1.3em;
+    line-height: 1em;
   }
 `
 
@@ -26,27 +61,29 @@ export default function JoinUs() {
       基本は月・水・金曜日の放課後
 
       <h2>FAQ</h2>
-      <Faq>
-        <Q>自分のPCは必要ですか？</Q>
-        <A>部室、演習室にあるPCを利用できるので、必要ありません。しかし、自分のPCがあると学生生活でも便利です。</A>
-      </Faq>
-      <Faq>
-        <Q>パソコンは好きだけど，知識は全然ありません。大丈夫ですか？</Q>
-        <A>大丈夫です！部活では、コンテストに出場し、経験を積みながら知識をつけることができます。<br />
-          先輩に質問したり、教えてもらいながら活動しましょう！</A>
-      </Faq>
-      <Faq>
-        <Q>兼部はOKですか？</Q>
-        <A>OKです！</A>
-      </Faq>
-      <Faq>
-        <Q>プログラミングや作曲などを学ぶ際に，一定のカリキュラムのようなものがありますか？</Q>
-        <A>１年生の間にプログラミング講習会を行います。</A>
-      </Faq>
-      <Faq>
-        <Q>部費はいくらですか？</Q>
-        <A>1年間で3000円から5000円程度です。</A>
-      </Faq>
+      <FaqList>
+        <details>
+          <Q>自分のPCは必要ですか？</Q>
+          <A>部室、演習室にあるPCを利用できるので、必要ありません。しかし、自分のPCがあると学生生活でも便利です。</A>
+        </details>
+        <details>
+          <Q>パソコンは好きだけど，知識は全然ありません。大丈夫ですか？</Q>
+          <A>大丈夫です！部活では、コンテストに出場し、経験を積みながら知識をつけることができます。<br />
+            先輩に質問したり、教えてもらいながら活動しましょう！</A>
+        </details>
+        <details>
+          <Q>兼部はOKですか？</Q>
+          <A>OKです！</A>
+        </details>
+        <details>
+          <Q>プログラミングや作曲などを学ぶ際に，一定のカリキュラムのようなものがありますか？</Q>
+          <A>１年生の間にプログラミング講習会を行います。</A>
+        </details>
+        <details>
+          <Q>部費はいくらですか？</Q>
+          <A>1年間で3000円から5000円程度です。</A>
+        </details>
+      </FaqList>
     </Layout>
   )
 }
