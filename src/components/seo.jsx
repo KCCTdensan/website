3761e08fa84862696fee12814f9e431038df727e
YamaD <me@dyama.net>
2021-05-25 13:21:44 +0900

    SEOとか

--- src/components/seo.jsx
+++ src/components/seo.jsx
@@ -2,13 +2,15 @@ import React from "react"
 import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
-export default function Seo({ title="", description="", lang="ja", meta=[] }) {
+export default function Seo({ title="", description="", keywords="", lang="ja", meta=[] }) {
   const { site } = useStaticQuery(graphql`
     query {
       site {
         siteMetadata {
           title
           description
+          author
+          keywords
         }
       }
     }
@@ -17,6 +19,8 @@ export default function Seo({ title="", description="", lang="ja", meta=[] }) {
   const siteTitle = site.siteMetadata.title
   const pageTitle = title
   const pageDescription = description || site.siteMetadata.description
+  const siteAuthor = site.siteMetadata.author
+  const pageKeywords = site.siteMetadata.keywords + (keywords ? `,${keywords}` : "")
 
   return (
     <Helmet
@@ -24,38 +28,20 @@ export default function Seo({ title="", description="", lang="ja", meta=[] }) {
       title={pageTitle}
       titleTemplate={`%s | ${siteTitle}`}
       meta={[
-        {
-          name: "description",
-          content: pageDescription
-        },
-        {
-          property: "og:title",
-          content: pageTitle
-        },
-        {
-          property: "og:description",
-          content: pageDescription
-        },
-        {
-          property: "og:type",
-          content: "website"
-        },
-        {
-          property: "og:image",
-          content: "https://d3bu.net/assets/icon.png"
-        },
-        {
-          name: "twitter:card",
-          content: "summary"
-        },
-        {
-          name: "twitter:title",
-          content: pageTitle
-        },
-        {
-          name: "twitter:description",
-          content: pageDescription
-        }
+        { name: "title",        content: pageTitle        },
+        { name: "description",  content: pageDescription  },
+        { name: "author",       content: siteAuthor       },
+        { name: "keywords",     content: pageKeywords     },
+
+        { property: "og:title",       content: pageTitle        },
+        { property: "og:description", content: pageDescription  },
+        { property: "og:site_name",   content: siteTitle        },
+        { property: "og:type",        content: "website"        },
+        { property: "og:image",       content: "https://d3bu.net/assets/icon.png" },
+
+        { name: "twitter:title",      content: pageTitle        },
+        { name: "twitter:description",content: pageDescription  },
+        { name: "twitter:card",       content: "summary"        },
       ].concat(meta)}
     />
   )
