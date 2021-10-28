b057b765a68a30e5401f3c8abe1c7baff51ee0b2
YamaD <me@dyama.net>
2021-10-29 08:52:00 +0900

    a

--- src/pages/osdev/index.jsx
+++ src/pages/osdev/index.jsx
@@ -1,13 +1,17 @@
 import React from "react"
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+import Layout from "../../components/layout"
+import Seo from "../../components/seo"
 
 const Osdev = () => {
   return (
     <Layout>
       <Seo title="OS開発" />
+      <h1>OS開発</h1>
+      <p>高専生なら1度は必ず挑戦すると言われている自作OSを部活で制作しています。</p>
+
       <h2>ハリボテック</h2>
+      <p>まだ完成していません。</p>
     </Layout>
   )
 }
