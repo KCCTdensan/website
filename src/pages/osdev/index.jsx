ae797006c9967d716876136dac3d35998056d928
YamaD <me@dyama.net>
2021-10-29 08:43:30 +0900

    a

---
+++ src/pages/osdev/index.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const Osdev = () => {
+  return (
+    <Layout>
+      <Seo title="OS開発" />
+      <h2>ハリボテック</h2>
+    </Layout>
+  )
+}
+
+export default Osdev
