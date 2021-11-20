500e528cd68def3c290aea1337269f0d344f645b
YamaD <me@dyama.net>
2021-11-20 23:29:23 +0900

    ?

--- src/mdLayout.svelte
+++ src/mdLayout.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
 	import Meta from "$lib/Meta.svelte"
 
-	export let title         : string = "\"hello, world\""
-	export let description   : string = "神戸高専電算部のウェブサイト"
-	export let author        : string = "KCCTdensan"
-	export let noTitleFormat : bool   = false
+	export let title         = "\"hello, world\""
+	export let description   = "神戸高専電算部のウェブサイト"
+	export let author        = "KCCTdensan"
+	export let noTitleFormat = false
 </script>
 
 <Meta { ...{ title, description, author, noTitleFormat } } />
