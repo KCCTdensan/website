d699afb3c12c2cc668de2d9f32b8cde7b46689ff
YamaD <me@dyama.net>
2021-11-21 11:38:25 +0900

    ヘッダーフッターと100vhのアレ

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,14 +1,11 @@
-const tailwindcss = require("tailwindcss")
-const autoprefixer = require("autoprefixer")
-const cssnano = require("cssnano")
-
 const mode = process.env.NODE_ENV
 const dev = mode === "development"
 
 module.exports = {
 	plugins: [
-		tailwindcss(),
-		autoprefixer(),
-		!dev && cssnano({ preset: "default" }),
+		require("tailwindcss"),
+		require("postcss-viewport-height-correction"),
+		require("autoprefixer"),
+		!dev && require("cssnano")({ preset: "default" }),
 	],
 }
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -16,8 +16,8 @@
 		<!-- center -->
 		<div class="flex flex-col justify-center">
 			<div class="mx-auto flex">
-				<div>(ぎてゅぶアイコン)</div>
-				<div>(Twitterアイコン)</div>
+				<a rel="external" href="https://github.com/KCCTdensan">(ぎてゅぶアイコン)</a>
+				<a rel="external" href="https://twitter.com/intent/user?user_id=444436310">(Twitterアイコン)</a>
 			</div>
 			<small class="mx-auto">&copy; 1974-{ new Date().getFullYear() } KCCTdensan 神戸高専電算部</small>
 		</div>
