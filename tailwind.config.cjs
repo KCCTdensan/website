9189d3bcd528e887d155a60f77a34e4f0288ab4c
YamaD <me@dyama.net>
2021-12-02 21:14:57 +0900

    アップデートといろいろ

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,11 @@
 module.exports = {
 	mode: "jit",
 	purge: ["./src/**/*.{html,js,svelte,ts}"],
+	darkMode: "class",
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [
+		require("@themesberg/flowbite/plugin")
+	],
 }
