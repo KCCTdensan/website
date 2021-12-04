27af8185ed2a047a60a4e6e9533baca9e9ecf41a
YamaD <me@dyama.net>
2021-12-04 20:33:48 +0900

    なんか

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -2,5 +2,10 @@ const mode = process.env.NODE_ENV
 const dev = mode === "development"
 
 module.exports = {
-  plugins: [require("tailwindcss"), require("postcss-viewport-height-correction"), require("autoprefixer"), !dev && require("cssnano")({ preset: "default" })],
+  plugins: [
+    require("tailwindcss"),
+    require("postcss-viewport-height-correction"),
+    require("autoprefixer"),
+    !dev && require("cssnano")({ preset: "default" }),
+  ],
 }
