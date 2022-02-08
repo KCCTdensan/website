467d7f0fad65868cd0130a9a6b860a8b69fcc0f9
YamaD <me@dyama.net>
2022-02-08 18:36:23 +0900

    なんちゃってBFF

--- src/routes/api/assets/[...path].ts
+++ src/routes/api/assets/[...path].ts
@@ -1,7 +1,7 @@
 import { API_BASE } from "$lib/_env"
 
 export async function get({ params }) {
-  const res = await fetch(`${API_BASE}/assets/${params.dir}/${params.file}`)
+  const res = await fetch(`${API_BASE}/assets/${params.path}`)
   return {
     status: res.status,
     body: new Uint8Array(await res.arrayBuffer()),
