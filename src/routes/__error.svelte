350654be7f016b71ccb90a926c7a225f096230f4
YamaD <me@dyama.net>
2022-03-03 18:32:02 +0900

    エラーページの更新

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -6,21 +6,22 @@
 
 <script lang="ts">
   import Meta from "$lib/meta.svelte"
-  export let status : int     = undefined;
-  export let error  : string  = undefined;
+  export let status : int     = undefined
+  export let error  : string  = undefined
 </script>
 
 <Meta title="Error_{status}" description="エラーが発生しました．" />
 
 <h1>
-<pre><code><span class="text-red-600">error:</span> {error.message}
-  x | assert(<span class="text-red-600">status != {status}</span>);
-    |        <span class="text-red-600">~~~~~~~^~~~~~</span></code>
-</pre>
+<pre><code><span class="red">error:</span> {error.message}
+  x | static_assert(<span class="red">status != {status}</span>);
+    |               <span class="red">~~~~~~~^~~~~~</span></code></pre>
 </h1>
 
 <p>
-  <!--{#if status === 404}
+  {#if status === 404}
     お探しのページは見つかりませんでした……ごめんなさい………
-  {/if}-->
+  {:else}
+    エラーが発生しました．
+  {/if}
 </p>
