e7023902a4a995cf837fd9c6e5df7845efce9e86
YamaD <me@dyama.net>
2022-03-03 16:01:46 +0900

    layoutのベースをscssで書き直し

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,56 +1,82 @@
 <script lang="ts">
-  import "../app.scss"
-
   import Icon from "svelte-awesome"
   import { github, twitter } from "svelte-awesome/icons"
-
-  import { hamburgerrrrr } from "$lib/stores"
-
-  import Header from "$lib/header/Header.svelte"
+  // import { hamburgerrrrr } from "$lib/stores"
+  // import Header from "$lib/header/Header.svelte"
+  import "../app.scss"
 </script>
 
-<div class="min-h-screen flex">
-  <div class="lg:my-5 container flex flex-col">
-    <Header />
-    <div class="flex-grow flex flex-col bg-mainBG text-mainText drop-shadow-2xl">
-      <main class="px-1 md:px-4 pt-6 flex flex-col animate-fadeIn">
-        <slot>Hello!</slot>
-      </main>
-      <footer class="mt-auto justify-between">
-        <!-- left -->
-        <div></div>
-        <!-- center -->
-        <div class="flex flex-col justify-center">
-          <div class="mx-auto mt-8 w-14 flex justify-between">
-            <a
-              href="https://github.com/KCCTdensan"
-              rel="external"
-              target="_blank"
-              class="h-6 w-6 inline-block text-gray-600"
-            ><Icon class="w-full h-full" data={github} /></a>
-            <a
-              href="https://twitter.com/intent/user?user_id=444436310"
-              rel="external"
-              target="_blank"
-              class="h-6 w-6 inline-block text-gray-600"
-            ><Icon class="w-full h-full" data={twitter} /></a>
-          </div>
-          <small
-            class="mx-auto mb-4 mt-2 text-base font-bold"
-          >&copy; 1974-{ new Date().getFullYear() } KCCTdensan 神戸高専電算部</small>
+<div class="app">
+  <div class="container">
+    <header class="header">
+      <!-- <Header /> -->
+    </header>
+    <main class="main">
+      <slot>Hello!</slot>
+    </main>
+    <footer class="footer">
+      <!-- left -->
+      <div></div>
+      <!-- center -->
+      <div class="flex-col">
+        <div class="iconLinks">
+          <a class="smallIcon" href="https://github.com/KCCTdensan" target="_blank" rel="external">
+            <Icon class="icon" data={github} />
+          </a>
+          <a class="smallIcon" href="https://twitter.com/intent/user?user_id=444436310" target="_blank" rel="external">
+            <Icon class="icon" data={twitter} />
+          </a>
         </div>
-        <!-- right -->
-        <div>
-          <!-- 背景にabsoluteな何かを置きたい 斜めなロゴとか -->
-        </div>
-      </footer>
-    </div>
+        <small class="copyright">&copy; 1974-{ new Date().getFullYear() } KCCTdensan 神戸高専電算部</small>
+      </div>
+      <!-- right -->
+      <div>
+        <!-- 背景にabsoluteな何かを置きたい 斜めなロゴとか -->
+      </div>
+    </footer>
   </div>
 </div>
 
 <style lang="scss">
+  @import "../styles/helpers.scss";
+  @import "../styles/colors.scss";
+
+  .app {
+    @extend .flex-row;
+    min-height: 100vh;
+    background-color: $c-ground;
+  }
+
   .container {
-    margin: 0 auto;
-    max-width: 840px;
+    @extend .flex-col;
+    width: 840px;//
+    margin: 40px auto;//
+    padding: 40px;//
+    background-color: $c-bg;
+    color: $c-text;
+  }
+
+  .header {}
+
+  .main {}
+
+  .footer {
+    // justify-center
+
+    .iconLinks {
+      // w-100 justify-center
+      // mx-auto mt-8 w-14 flex justify-between
+    }
+  }
+
+  .smallIcon {
+    // h-6 w-6 inline-block text-gray-600
+    .icon {
+      // w-full h-full
+    }
+  }
+
+  .copyright {
+    // mx-auto mb-4 mt-2 text-base font-bold
   }
 </style>
