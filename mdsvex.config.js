482c1c1bd8a5f40cfa1400b9dcf35cd20d84f4b8
YamaD <me@dyama.net>
2022-03-08 22:55:15 +0900

    markdownの数式対応

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -1,4 +1,8 @@
-export default {
+import { defineMDSveXConfig as defineConfig } from "mdsvex"
+import remarkMath from "remark-math"
+import rehypeKatex from "rehype-katex-svelte"
+
+export default defineConfig({
   extensions: [".md", ".svx"],
   smartypants: {
     dashes: "oldschool",
@@ -6,4 +10,6 @@ export default {
   layout: {
     _: "./src/md_layout.svelte",
   },
-}
+  remarkPlugins: [remarkMath],
+  rehypePlugins: [rehypeKatex],
+})
