cb545c134d4e7f52e6a91820ac42b944c35b5cca
YamaD <me@dyama.net>
2022-03-10 12:20:10 +0900

    fix $PLATFORM

--- src/lib/_env.ts
+++ src/lib/_env.ts
@@ -2,4 +2,4 @@ export const API_BASE: string = process.env.API_BASE
 export const DEV: boolean = process.env.DEV === "true"
 export const SSR: boolean = process.env.SSR === "true"
 export const PLATFORM: string =
-  process.env.PLATFORM ?? DEV ? "dev" : SSR ? "unknown-ssr" : "unknown"
+  process.env.PLATFORM || DEV ? "dev" : SSR ? "unknown-ssr" : "unknown"
