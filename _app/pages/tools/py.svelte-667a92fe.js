2630dc9a728a7ca7d15614904c4a012291b2ee2a
yamader <yamader@users.noreply.github.com>
2022-04-14 13:24:41 +0000

    deploy: 34cc27fe110d10d890376f5c64d2477b20795bfa

---
+++ _app/pages/tools/py.svelte-667a92fe.js
@@ -0,0 +1,1 @@
+import{S as s,i as e,s as o}from"../../chunks/index-0daf26e0.js";async function l(){return{status:302,redirect:"/old/v1/tools/py/"}}class r extends s{constructor(t){super(),e(this,t,null,null,o,{})}}export{r as default,l as load};
