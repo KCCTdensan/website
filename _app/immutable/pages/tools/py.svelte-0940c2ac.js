292cdb2e482cd940e8be3033f2a43e3027d5cfb1
yamader <yamader@users.noreply.github.com>
2022-07-18 11:53:56 +0000

    deploy: a163ecb088dccdd1d5bf4b33576dc187195aec34

--- _app/immutable/pages/tools/py.svelte-0940c2ac.js
+++
@@ -1,1 +0,0 @@
-import{S as s,i as e,s as o}from"../../chunks/index-ee3c2835.js";async function l(){return{status:302,redirect:"/old/v1/tools/py/"}}class r extends s{constructor(t){super(),e(this,t,null,null,o,{})}}export{r as default,l as load};
---
+++ _app/immutable/pages/tools/py.svelte-234137c5.js
@@ -0,0 +1,1 @@
+import{S as s,i as e,s as o}from"../../chunks/index-8e7dcfba.js";async function l(){return{status:302,redirect:"/old/v1/tools/py/"}}class r extends s{constructor(t){super(),e(this,t,null,null,o,{})}}export{r as default,l as load};
